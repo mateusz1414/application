f0ff9bc0743b3f1f7647c28fe9c1864b7eb757fa
mateusz1414 <mateus141423@gmail.com>
2021-01-22 20:18:05 +0100

    change adresses

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverAddress: "http://35.198.132.61:8080/",
+    serverAddress: "http://35.198.132.61/",
     //serverAddress: "http://34.89.252.178:8080/",
     //apiAddress: "http://34.89.252.178:8081/"
-    apiAddress: "http://35.198.132.61:8081/"
+    apiAddress: "http://35.198.132.61/api/"
 }
