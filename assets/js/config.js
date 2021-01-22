8967f89ceaba20e359f0389d716745f0aa6042f0
mateusz1414 <mateus141423@gmail.com>
2021-01-22 20:48:18 +0100

    update adresses

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverAddress: "http://35.198.132.61/",
+    serverAddress: "http://studentscode.online/",
     //serverAddress: "http://34.89.252.178:8080/",
     //apiAddress: "http://34.89.252.178:8081/"
-    apiAddress: "http://35.198.132.61/api/"
+    apiAddress: "http://studentscode.online/api/"
 }
